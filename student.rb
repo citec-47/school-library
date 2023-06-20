fdd79a2a9c0c8b08bc50831074bba26ffc407472
citec-47 <mauricendonyi40@gmail.com>
2023-06-19 18:59:14 -0700

    Add classes

--- student.rb
+++ student.rb
@@ -1,12 +1,12 @@
 require_relative './person'
 
 class Student < Person
-  def initialize(classroom, age, name = 'Unknow', parent_permission: true)
-    super(id, age, name, parent_permission: parent_permission)
+  def initialize(classroom, age, name = 'Unknown', parent_permission: true)
+    super(age, name, parent_permission: parent_permission)
     @classroom = classroom
   end
 
   def play_hooky
-    '¯(ツ)/¯'
+    '¯\(ツ)/¯'
   end
 end
