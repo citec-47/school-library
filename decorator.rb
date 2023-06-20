69626926ab9767095477b521c3f654b5e1ec9865
citec-47 <mauricendonyi40@gmail.com>
2023-06-20 07:28:43 -0700

    Adding nameable to person

--- decorator.rb
+++ decorator.rb
@@ -1,9 +1,12 @@
-require './nameable'
-
+require_relative 'nameable'
+# base decorator is implemented the store the wrapped method and
+# means the initialize or change them
 class Decorator < Nameable
+  attr_accessor :nameable
+
   def initialize(nameable)
-    super()
     @nameable = nameable
+    super()
   end
 
   def correct_name
