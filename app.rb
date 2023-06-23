6fea565f0a9f0dfbbc1aa09b8b4921fc569c31af
citec-47 <mauricendonyi40@gmail.com>
2023-06-22 20:08:19 -0700

    Editing app.rb

--- app.rb
+++ app.rb
@@ -6,10 +6,8 @@ require_relative 'rentals'
 class App
   def initialize
     @books = []
-    # @teacher = []
-    # @student = []
     @people = []
-    @rental = []
+    @rentals = []
   end
 
   def create_person
@@ -24,7 +22,7 @@ class App
       puts 'Invalid option'
       create_person
     end
-    puts 'Person created succesfully'
+    puts 'Person created successfully'
   end
 
   def create_student
@@ -40,7 +38,7 @@ class App
                    when 'n'
                      false
                    else
-                     puts 'It\'s a yes or no type thing!. c\'mon now! '
+                     puts 'It\'s a yes or no type thing! C\'mon now!'
                    end
     student = Student.new(student_age, student_name, is_permitted)
     @people.push(student)
@@ -59,9 +57,9 @@ class App
 
   def list_people
     if @people.empty?
-      puts 'Must a person first'
+      puts 'No people to display'
     else
-      @people.each { |person| puts "#{[person.class]} Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
+      @people.each { |person| puts "#{person.class}: Name: #{person.name}, ID: #{person.id}, Age: #{person.age}" }
     end
   end
 
@@ -77,50 +75,46 @@ class App
 
   def list_books
     if @books.empty?
-      puts 'There is no book yet'
+      puts 'No books to display'
     else
       @books.each { |book| puts "Title: #{book.title}, Author: #{book.author}" }
     end
   end
 
   def create_rental
-    if @books.size.positive? && @people.size.positive?
+    if @books.empty? || @people.empty?
+      puts 'There must be at least one book and one person for a rental to take place!'
+    else
       puts 'Select a book from the following list by number'
       @books.each_with_index { |book, index| puts "#{index}) Title: \"#{book.title}\", Author: #{book.author}" }
-      id = gets.chomp.to_i
-      puts 'Select a person from the following list by number (not id)'
+      book_id = gets.chomp.to_i
+      puts 'Select a person from the following list by number (not ID)'
       @people.each_with_index do |person, index|
-        puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, #{person.age}"
+        puts "#{index}) [#{person.class}] Name: #{person.name}, ID: #{person.id}, Age: #{person.age}"
       end
       person_id = gets.chomp.to_i
       puts 'Enter the date in this format yy/mm/dd: '
       date = gets.chomp
-      person_rental = Rental.new(@books[id], @people[person_id], date)
-      @rental.push(person_rental)
-      puts 'Rental creared successfully'
-    else
-      puts 'There must be a person and some book for a rental to take place!'
+      rental = Rental.new(@books[book_id], @people[person_id], date)
+      @rentals.push(rental)
+      puts 'Rental created successfully'
     end
   end
 
   def rental_list
-    if @rental.empty?
-      puts 'Rentals doesn\'t exist yet! come back later'
+    if @rentals.empty?
+      puts 'No rentals available'
     else
-      print 'Enter the person_id you wanna get the rental for: '
+      puts 'Enter the person ID to get their rentals: '
       person_id = gets.chomp.to_i
-      list_rental(person_id)
-    end
-  end
-
-  def list_rental(person_id)
-    person_rentals = @rental.select { |rental| rental.person.id == person_id }
-    if person_rentals.length.positive?
-      person_rentals.each do |rental|
-        puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+      person_rentals = @rentals.select { |rental| rental.person.id == person_id }
+      if person_rentals.empty?
+        puts 'The selected person has no rentals'
+      else
+        person_rentals.each do |rental|
+          puts "Date: #{rental.date}, Book \"#{rental.book.title}\" by #{rental.book.author}"
+        end
       end
-    else
-      puts 'The selected person has no rentals'
     end
   end
 
@@ -135,16 +129,14 @@ class App
     puts '7. Exit app'
 
     option_id = gets.chomp
-    puts option_id
     run_app(option_id)
   end
 
   def exit_app
-    puts 'Thank you for using this app! '
+    puts 'Thank you for using this app!'
     exit
   end
 
-  # rubocop:disable Metrics/CyclomaticComplexity
   def run_app(option_id)
     case option_id
     when '1'
@@ -162,14 +154,16 @@ class App
     when '7'
       exit_app
     else
-      puts 'Choose an existing option!..'
+      puts 'Choose an existing option!'
     end
     display_options
   end
-  # rubocop:enable Metrics/CyclomaticComplexity
 
   def initial
-    puts 'Welcome to School Library App!'
+    puts 'Welcome to the School Library App!'
     display_options
   end
 end
+
+app = App.new
+app.initial
