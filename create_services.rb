d3ac5aeb189205785616b6b30f38ea609b8ae33a
citec-47 <mauricendonyi40@gmail.com>
2023-06-26 05:12:27 -0700

    Fixing Lnters

--- create_services.rb
+++ create_services.rb
@@ -1,4 +1,3 @@
-# rubocop:disable Layout/EndOfLine
 def create_person
   print 'Do you want to create a student (1) or a teacher (2)? [Input the number]:'
   option = gets.chomp
@@ -13,7 +12,6 @@ def create_person
   end
   puts 'Person created succesfully'
 end
-# rubocop:enable Layout/EndOfLine
 
 def create_student
   print 'Age: '
