058022b8fed1bf6a90bf5f00cde111c4bb3702bc
citec-47 <mauricendonyi40@gmail.com>
2023-06-26 03:54:18 -0700

    fix

--- student.rb
+++ student.rb
@@ -4,16 +4,21 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(age, name, parent_permission)
-    super(age, name, parent_permission)
+    @age = age
+    @name = name
+    @parent_permission = parent_permission
     @id = Random.rand(1..1000)
+    super(age, name, parent_permission)
   end
 
   def play_hooky
     '¯\(ツ)/¯'
   end
 
+  # rubocop:disable Lint/DuplicateMethods
   def classroom=(classroom)
     @classroom = classroom
     classroom.number_of_students.push(self) unless classroom.number_of_students.include?(self)
   end
+  # rubocop:enable Lint/DuplicateMethods
 end
