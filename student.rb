88e06ecb7afdfbb117a658a2d9dbd3e84a28a54c
citec-47 <mauricendonyi40@gmail.com>
2023-06-26 03:37:40 -0700

    fixing linters

--- student.rb
+++ student.rb
@@ -4,11 +4,8 @@ class Student < Person
   attr_accessor :classroom
 
   def initialize(age, name, parent_permission)
-    @age = age
-    @name = name
-    @parent_permission = parent_permission
-    @id = Random.rand(1..1000)
     super(age, name, parent_permission)
+    @id = Random.rand(1..1000)
   end
 
   def play_hooky
